b4d9d79db192db6b12a0fd820116790828487127
ahmadumarse02 <ahmadumarse02@gmail.com>
2024-10-22 20:53:43 +0500

    to do app

---
+++ T-Do-Lists/script.js
@@ -0,0 +1,28 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTasks() {
+  if (inputBox.value === "") {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+}
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "li") {
+      e.target.classList.toggle(".clicked");
+    } else if (e.target.tagName === "span") {
+      e.target.parentElement.remove();
+    }
+  },
+  false
+);
